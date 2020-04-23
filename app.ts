9c69bf6562a8e6846abb458a6410851a463c2492
aaaaninja <aaaaninja@aaaa.ninja>
2020-04-23 17:08:33 +0900

    こっちのほうがべんり

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 import * as puppeteer from 'puppeteer'
 
 (async () => {
-  const browser = await puppeteer.launch({headless: false})
+  const browser = await puppeteer.launch({devtools: true})
   const page = await browser.newPage()
 
   await page.goto('https://www.dospara.co.jp/5shopping/search.php?ft=axf&gosearch=%E6%A4%9C%E7%B4%A2')
