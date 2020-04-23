4177a7c37899af11ba5e180a7642b0fd9dc4ba2a
aaaaninja <aaaaninja@aaaa.ninja>
2020-04-23 16:35:25 +0900

    目的のドスパラのサイトに行く

--- app.ts
+++ app.ts
@@ -2,5 +2,9 @@ import * as puppeteer from 'puppeteer'
 
 (async () => {
   const browser = await puppeteer.launch({headless: false})
+  const page = await browser.newPage()
+
+  await page.goto('https://www.dospara.co.jp/5shopping/search.php?ft=axf&gosearch=%E6%A4%9C%E7%B4%A2')
+
   await browser.close()
 })()
