9cb102540480cb98b5c2ad87fa4f15ca4bdc7a09
aaaaninja <aaaaninja@aaaa.ninja>
2020-04-23 17:34:15 +0900

    :tada: 監視script一応完成
    ほんとは `?.` 記法つかえばもっと可読性いいんだけど、bundleに使っている `esbuild` だと使えない…… (typescriptと併用した場合)

--- app.ts
+++ app.ts
@@ -5,6 +5,12 @@ import * as puppeteer from 'puppeteer'
   const page = await browser.newPage()
 
   await page.goto('https://www.dospara.co.jp/5shopping/search.php?ft=axf&gosearch=%E6%A4%9C%E7%B4%A2', { waitUntil: 'domcontentloaded' })
+  const axf = await page.$eval('#tabContents2', table => {
+    const axf_header = [...table.querySelectorAll('tr')].find(v => v.textContent && v.textContent.match(/アウトレット/))
+    const is_axf = axf_header && axf_header.nextElementSibling && [...axf_header.nextElementSibling.querySelectorAll('td')].find(v => v.textContent && v.textContent.match(/在庫切れ/))
+    return is_axf ? is_axf.textContent && is_axf.textContent.trim() : 'hukkatsu'
+  })
+  console.log(axf)
 
   await browser.close()
 })()
